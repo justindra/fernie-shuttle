07dd4fc895f44264e430313c9bf1f95535bc673d
Justin Rahardjo <justindra91@gmail.com>
2019-11-13 17:07:54 -0700

    chore: fix formatting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  testMatch: [
-    '**/src/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'
-  ]
+  testMatch: ['**/src/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)']
 };
