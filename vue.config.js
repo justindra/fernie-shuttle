c5977694c827336fbc8f861b5d83a3ebb3869676
Justin Rahardjo <justindra91@gmail.com>
2019-11-28 16:57:30 -0700

    chore: add vue-config for GH Pages deployment

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // Set public path for Github Pages Deployment
+  // https://cli.vuejs.org/guide/deployment.html#github-pages
+  publicPath: process.env.NODE_ENV === 'production' ? '/fernie-shuttle/' : '/'
+};
