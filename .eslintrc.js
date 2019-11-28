35cf20146e69954c1cb7cf42b33d1ab35965d4cc
Justin Rahardjo <justindra91@gmail.com>
2019-11-28 16:30:09 -0700

    chore: allow console error and warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript', 'prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console':
+      process.env.NODE_ENV === 'production' ? ['error', { allow: ['warn', 'error'] }] : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-useless-constructor.md#rule-changes
     'no-useless-constructor': 'off',
