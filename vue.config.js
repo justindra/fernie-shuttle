15f692c9b25fd3b4c22fc528dd8ecef63ac91c45
Justin Rahardjo <justindra91@gmail.com>
2019-12-05 20:26:50 -0700

    chore: add PWA configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   // Set public path for Github Pages Deployment
   // https://cli.vuejs.org/guide/deployment.html#github-pages
-  publicPath: process.env.NODE_ENV === 'production' ? '/fernie-shuttle/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/fernie-shuttle/' : '/',
+  // Progressive Web App Configurations
+  pwa: {
+    name: 'Fernie Ski Shuttle',
+    themeColor: '#448aff'
+  }
 };
