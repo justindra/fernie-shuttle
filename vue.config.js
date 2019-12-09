87a74e6cbeb6ff4b6257cde17f9d4bcbd025ac45
Justin Rahardjo <justindra91@gmail.com>
2019-12-09 12:16:43 -0700

    chore: updated to work with custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // Set public path for Github Pages Deployment
-  // https://cli.vuejs.org/guide/deployment.html#github-pages
-  publicPath: process.env.NODE_ENV === 'production' ? '/fernie-shuttle/' : '/',
   // Progressive Web App Configurations
   pwa: {
     name: 'Fernie Ski Shuttle',
