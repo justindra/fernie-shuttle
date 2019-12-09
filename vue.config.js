77beb0fb1b70d044d6aacb74729d4e9b80532fc3
Justin Rahardjo <justindra91@gmail.com>
2019-12-09 10:56:59 -0700

    chore: update the logo (#9)
    * chore: updated logos and manifest
    
    * update background color in manifest

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,16 @@ module.exports = {
   // Progressive Web App Configurations
   pwa: {
     name: 'Fernie Ski Shuttle',
-    themeColor: '#448aff'
+    themeColor: '#448aff',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
+    },
+    manifestOptions: {
+      background_color: '#ffffff'
+    }
   }
 };
